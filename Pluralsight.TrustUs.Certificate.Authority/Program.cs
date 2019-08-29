b951d613c948c85ba4ad5e99c6fa8ce674f23c57
Ed <ed@s.com>
2019-08-29 19:11:17 +0000

    TrustUs code fix

--- Pluralsight.TrustUs.Certificate.Authority/Program.cs
+++ Pluralsight.TrustUs.Certificate.Authority/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel;
+﻿using System;
 using cryptlib;
 
 namespace Pluralsight.TrustUs
@@ -9,9 +9,22 @@ namespace Pluralsight.TrustUs
         {
             crypt.Init();
 
+            var done = false;
             var ca = new CertificateAuthority();
-            ca.Install();
 
+            do
+            {
+                switch(DisplayMenu())
+                {
+                    case 1:
+                        ca.Install();
+                        break;
+                    case 5:
+                        done = true;
+                        break;
+                }
+            } while (!done);
+                      
             //var keysetOpen = crypt.KeysetOpen(crypt.UNUSED, crypt.KEYSET_FILE, @"C:\Pluralsight\Keys\ica.keys", crypt.KEYOPT_READONLY);
             //var cert = crypt.GetPublicKey(keysetOpen, crypt.KEYID_NAME, "TrustUsIcaKeyPair");
             //var size = crypt.ExportCert(null, 0, crypt.CERTFORMAT_CERTCHAIN, cert);
@@ -20,7 +33,20 @@ namespace Pluralsight.TrustUs
             //System.IO.File.WriteAllBytes(@"C:\Pluralsight\Keys\ica.cer", certificate);
             //crypt.KeysetClose(keysetOpen);
 
-            crypt.End();
+            crypt.End(); 
+        }
+
+        static public int DisplayMenu()
+        {
+            Console.WriteLine("TrustUs Certificate Authority");
+            Console.WriteLine();
+            Console.WriteLine("1. Install Certificate Authority");
+            Console.WriteLine("2. List the Football teams");
+            Console.WriteLine("3. Search for a Football team");
+            Console.WriteLine("4. Delete a team");
+            Console.WriteLine("5. Exit");
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
         }
     }
 }
